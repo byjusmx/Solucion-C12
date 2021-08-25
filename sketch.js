50d45eb472fff8853b2ff0936f38a8e0c75d0721
byjusmx <87218747+byjusmx@users.noreply.github.com>
2021-08-25 10:40:22 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,12 +14,12 @@ function preload(){
 function setup(){
   
   createCanvas(400,400);
-// Moving background
+// Fondo en movimiento
 garden=createSprite(200,200);
 garden.addImage(gardenImg);
 
 
-//creating boy running
+//Creando niño corriendo
 rabbit = createSprite(160,340,20,20);
 rabbit.scale =0.09;
 rabbit.addImage(rabbitImg);
@@ -28,7 +28,7 @@ rabbit.addImage(rabbitImg);
 function draw() {
   background(0);
   
-  // boy moving on Xaxis with mouse'
+  // Niño moviéndose en el eje X con el mouse
   rabbit.x = World.mouseX;
   
   edges= createEdgeSprites();
